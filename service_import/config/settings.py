7ab5ae99546dd632849ca55f2ff290a5e0462267
Papa Magatte Diop <papamagatte8diop@gmail.com>
2026-04-15 17:17:59 +0000

    Enable WhiteNoise static serving for service_import in production

--- service_import/config/settings.py
+++ service_import/config/settings.py
@@ -24,6 +24,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -94,6 +96,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 STATIC_URL  = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'frontend' / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL   = '/media/'
 MEDIA_ROOT  = BASE_DIR / 'media'
 
