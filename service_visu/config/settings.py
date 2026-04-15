83dc750e59a362e654fd406b965d8e027f8c2f83
Papa Magatte Diop <papamagatte8diop@gmail.com>
2026-04-15 20:14:38 +0000

    Handle blank ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS in service_visu settings

--- service_visu/config/settings.py
+++ service_visu/config/settings.py
@@ -9,7 +9,12 @@ SECRET_KEY = os.environ.get('SECRET_KEY_VISU', 'dev-secret-service_visu')
 
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
+ALLOWED_HOSTS = [
+    host.strip() for host in os.environ.get('ALLOWED_HOSTS', '*').split(',') if host.strip()
+]
+CSRF_TRUSTED_ORIGINS = [
+    origin.strip() for origin in os.environ.get('CSRF_TRUSTED_ORIGINS', '').split(',') if origin.strip()
+]
 
 
 def normalize_service_url(value, default):
